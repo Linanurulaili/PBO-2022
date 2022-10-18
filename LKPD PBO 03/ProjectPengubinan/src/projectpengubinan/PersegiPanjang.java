7025918427ba6793e320f85517db475410ff27bb
Lina Nurullaili_K3521039 <89761692+Linanurulaili@users.noreply.github.com>
2022-10-18 13:14:52 +0700

    Add files via upload

---
+++ LKPD PBO 03/ProjectPengubinan/src/projectpengubinan/PersegiPanjang.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectpengubinan;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class PersegiPanjang {
+    // atribut
+   static int perkalian(int a, int b){
+        int hasil = a * b;
+        return hasil;
+    }
+    
+    //method 33
+    int hitungLuas(int panjang, int lebar){
+        int luas = perkalian(panjang, lebar);
+        return luas;
+    }
+}
